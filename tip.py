95254bf065e0278cf7b5c8ff02596bc7e600bee4
Gaurav <tgaurav2121@gmail.com>
2026-04-12 21:49:02 +0530

    Calculate a tip based on user input for the cost of a meal and the desired tip percentage.

---
+++ tip.py
@@ -0,0 +1,19 @@
+#Calculate a tip based on user input for the cost of a meal and the desired tip percentage.
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    d = d.replace("$","")
+    d = float(d)
+    return d
+
+def percent_to_float(p):
+    p = p.replace("%","")
+    p = float(p) / 100
+    return p
+
+main()
