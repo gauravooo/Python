0dffb31681441a55b2f9addad8cf0493d9c12ade
Gaurav <tgaurav2121@gmail.com>
2026-04-27 16:56:00 +0530

    Camel Case

---
+++ camel.py
@@ -0,0 +1,10 @@
+def main():
+    camelcase = input("Camelcase: ").strip()
+
+    for i in camelcase:
+        if i == i.upper():
+            print(f"_{i.lower()}",end ="")
+        else:
+            print(i,end = "")
+    print(end ="\n")
+main()
